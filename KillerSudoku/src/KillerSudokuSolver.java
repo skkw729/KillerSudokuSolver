e1a030339713afd19a498c2e828196234532a851
Stephen <Stephen@DESKTOP-6KSAIB5.lan>
2017-07-10 10:11:24 +0100

    updated strategies
    merged

--- KillerSudoku/src/KillerSudokuSolver.java
+++ KillerSudoku/src/KillerSudokuSolver.java
@@ -16,28 +16,51 @@ public class KillerSudokuSolver {
 	
 	public List<SudokuCell> getCagesSpanningRegion(){
 		//rule of 45 - for each cage, check cell location
-		//check row
+		//check row, column and nonet
 		List<Cage> cages = grid.getCages();
 		Map<Integer, List<Cage>> rowMap = new HashMap<>();
 		Map<Integer, List<Cage>> columnMap = new HashMap<>();
 		Map<Integer, List<Cage>> nonetMap = new HashMap<>();
 		for(int i=1;i<=9;i++){
+			List<Cage> cageRowList = new ArrayList<>();
+			List<Cage> cageColumnList = new ArrayList<>();
+			List<Cage> cageNonetList = new ArrayList<>();
 			for(Cage c : cages){
-				List<Cage> cageList = new ArrayList<>();
 				List<Location> locations = c.getCells();
-				boolean same=true;
-				for(Location l : locations){
-					if(l.getRow()!=i) same = false;
+				boolean sameRow = true;
+				boolean sameColumn = true;
+				boolean sameNonet = true;
+				for(Location l : locations){//check if all cells within a cage are in the same location
+					if(l.getRow()!=i) sameRow = false;
+					if(l.getColumn()!=i) sameColumn = false;
+					if(l.getNonet()!=i) sameNonet = false;
 				}
-				if(same){
-					cageList.add(c);
+				if(sameRow){
+					cageRowList.add(c);
+				}
+				if(sameColumn){
+					cageColumnList.add(c);
+				}
+				if(sameNonet){
+					cageNonetList.add(c);
 				}
 			}
+			rowMap.put(i, cageRowList);
+			columnMap.put(i, cageColumnList);
+			nonetMap.put(i, cageNonetList);
 		}
-		//check column
-		//check nonet
+		//check length of cages in map
+		for(int i=1;i<=9;i++){
+		List<Cage> cageList = rowMap.get(i);
+		int span = 0;
+		for(Cage c : cageList){
+			span += c.getLength();
+		}
+		if(span == SIZE-1){}
+		}
 		
 		return new ArrayList<>();
 	}
-
 }
+		
+
