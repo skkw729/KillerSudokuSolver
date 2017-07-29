a155bddea1302d976a760e6ca3a6b1c7f22c7e73
Stephen <Stephen@DESKTOP-6KSAIB5.lan>
2017-07-29 13:45:09 +0100

    implemented greedy colouring for cages

--- KillerSudoku/src/view/SudokuCellUI.java
+++ KillerSudoku/src/view/SudokuCellUI.java
@@ -6,34 +6,50 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 
 @SuppressWarnings("serial")
 public class SudokuCellUI extends JPanel {
 	private static final int SIZE = 9;
-	private static final int CELL_SIZE = 50;
+	private static final int CELL_SIZE = 70;
 	private static final Font SOLVED_FONT = new Font("SansSerif", Font.BOLD, 30);
+	private static final Font TOTAL_FONT = new Font("Serif", Font.BOLD, 15);
 	private static final Font UNSOLVED_FONT = new Font("SansSerif", Font.PLAIN, 12);
-	private static Border COMPOUND_BORDER = BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.black), BorderFactory.createEmptyBorder(5,5,5,5));
+	private static Border COMPOUND_BORDER = BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.black), BorderFactory.createEmptyBorder(18,0,0,0));
 	private static Border BORDER = BorderFactory.createLineBorder(Color.black);
 	private Location location;
 	private SudokuCell cell;
-	public SudokuCellUI(SudokuCell cell){
+	private Color colour;
+	public SudokuCellUI(SudokuCell cell, Color colour){
 		super(new GridLayout(3,3));
 		this.cell = cell;
+		this.colour = colour;
 		location = cell.getLocation();
-		paint();
+		init();
 //		this.setPreferredSize(new Dimension(CELL_SIZE, CELL_SIZE));
+		
+	}
+	public SudokuCellUI(SudokuCell cell, Color colour, int cageTotal){
+		super(new GridLayout(3,3));
+		this.cell = cell;
+		this.colour = colour;
+		location = cell.getLocation();
+		init();
+		Border TITLED_BORDER = BorderFactory.createCompoundBorder(BORDER, BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(), ""+cageTotal, TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.LEFT, TOTAL_FONT));
+		this.setBorder(TITLED_BORDER);
+		//		this.setPreferredSize(new Dimension(CELL_SIZE, CELL_SIZE));
 		
 	}
 	public Dimension getPreferredSize(){
 		return new Dimension(CELL_SIZE, CELL_SIZE);
 	}
-	private void paint() {
+	private void init() {
 		if(!cell.isSolved()){
 			for(int i=1;i<=SIZE;i++){
 				String s = ""+i;
@@ -55,6 +71,10 @@ public class SudokuCellUI extends JPanel {
 			this.add(l);
 		}
 		this.setBorder(COMPOUND_BORDER);
+	}
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		super.setBackground(colour);
 	}
 	public SudokuCell getCell(){
 		return cell;
--- KillerSudoku/src/view/SudokuGridUI.java
+++ KillerSudoku/src/view/SudokuGridUI.java
@@ -1,10 +1,19 @@
 package view;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import model.*;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -14,16 +23,26 @@ public class SudokuGridUI {
 	private SudokuCellUI[][] gridUI;
 	private KillerSudokuGrid grid;
 	private List<Cage> cages;
+	private List<SudokuCellUI> listCellUI;
+	Map<Cage, Color> cageColourMap;
 	private static final int SIZE=9;
-	private static final int CELL_SIZE = 50;
+	private static final int CELL_SIZE = 70;
 	private JFrame frame;
 	private JPanel contentPanel;
+	private static Color RED = new Color(255, 80, 80);
+	private static Color YELLOW = new Color(255, 255, 153);
+	private static Color GREEN = new Color(153, 255, 153);
+	private static Color BLUE = new Color(153, 204, 255);
+	private static Color PINK = new Color(255, 204, 255);
 	public SudokuGridUI(KillerSudokuGrid grid){
 		this.grid = grid;
+		listCellUI = new ArrayList<>();
+		cageColourMap = new HashMap<>();
 		contentPanel = new JPanel(new BorderLayout());
 		frame = new JFrame("Killer Sudoku Solver");
 		frame.setLayout(new BorderLayout());
 		cages = grid.getCages();
+		assignAllCageColours();
 		makeGrid(grid);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
@@ -36,15 +55,146 @@ public class SudokuGridUI {
 		contentPanel.add(new ButtonPanel(), BorderLayout.EAST);
 		contentPanel.add(panel, BorderLayout.CENTER);
 		frame.setContentPane(contentPanel);
+		Map<SudokuCell, Integer> cageLeadMap = getCageLeads();
 		for(int i=1;i<=SIZE;i++){
+			JPanel nonetPanel = new JPanel(new GridLayout(3,3));
 			List<SudokuCell> nonet = grid.getNonet(i);
-			panel.add(new SudokuNonetUI(nonet));
+			for(SudokuCell cell : nonet){
+				Cage cage = grid.getCage(cell.getLocation());
+				Color colour = cageColourMap.get(cage);
+				if(cageLeadMap.get(cell) != null){
+					int cageTotal = cageLeadMap.get(cell);
+					SudokuCellUI sudokuCellUI = new SudokuCellUI(cell, colour, cageTotal);
+					listCellUI.add(sudokuCellUI);
+					nonetPanel.add(sudokuCellUI);
+				}
+				else{
+					SudokuCellUI sudokuCellUI = new SudokuCellUI(cell, colour);
+					listCellUI.add(sudokuCellUI);
+					nonetPanel.add(sudokuCellUI);
+				}
+				nonetPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
+			}
+			panel.add(nonetPanel);
 		}
-//		frame.setMinimumSize(new Dimension(SIZE*CELL_SIZE, SIZE*CELL_SIZE));
+		//		frame.setMinimumSize(new Dimension(SIZE*CELL_SIZE, SIZE*CELL_SIZE));
 		frame.revalidate();
 		frame.repaint();
-		
+
 	}
+	public Map<SudokuCell, Integer> getCageLeads(){
+		Map<SudokuCell, Integer> cageLeadMap = new HashMap<>();
+		for(Cage cage : cages){
+			int row = -1;
+			for(Location l : cage.getCellLocations()){
+				if(row==-1 || l.getRow()<row){
+					row = l.getRow();
+				}
+			}
+			int col = -1;
+			for(Location l : cage.getCellLocations()){
+				if(l.getRow()==row){
+					if(col==-1 || l.getColumn()<col){
+						col = l.getColumn();
+					}
+				}
+			}
+			SudokuCell cell = grid.getCell(Location.getInstance(row, col));
+			int cageTotal = cage.getTotal();
+			cageLeadMap.put(cell, cageTotal);
+		}
+		return cageLeadMap;
+	}
+	public void assignAllCageColours(){
+		Map<Cage, List<Cage>> adjCages = getAdjacentCageMap();
+		for(Cage cage : cages){
+			assignColour(cage, adjCages);
+		}
+	}
+	private void assignColour(Cage cage, Map<Cage,List<Cage>> adjCages){
+		Color colour = RED;
+		List<Cage> adjacentCages = adjCages.get(cage);
+		if(adjacentCages != null){
+
+			boolean redUsed = false;
+			boolean blueUsed = false;
+			boolean yellowUsed = false;
+			boolean greenUsed = false;
+			for(Cage c : adjacentCages){
+				Color adjColor = cageColourMap.get(c);
+				if(adjColor != null){
+					if(adjColor.equals(RED)){
+						redUsed = true;
+					}
+					else if(adjColor.equals(BLUE)){
+						blueUsed = true;
+					}
+					else if(adjColor.equals(YELLOW)){
+						yellowUsed = true;
+					}
+					else if(adjColor.equals(GREEN)){
+						greenUsed = true;
+					}
+				}
+				//greedy colouring
+				if(redUsed){
+					colour = BLUE;
+					if(blueUsed){
+						colour = YELLOW;
+						if(yellowUsed){
+							colour = GREEN;
+							if(greenUsed){
+								colour = PINK;
+							}
+						}
+
+
+					}
+
+				}
+			}
+		}
+		cageColourMap.put(cage, colour);
+	}
+	private Map<Cage, List<Cage>> getAdjacentCageMap(){
+		Map<Cage, List<Cage>> adjacentCages = new HashMap<>();
+		for(Cage cage : cages){
+			List<Cage> cageList = new ArrayList<>();
+			Set<Cage> cageSet = new HashSet<>();
+			for(Location l : cage.getCellLocations()){
+				int column = l.getColumn();
+				int row = l.getRow();
+				if(column-1>0){ 
+					Location west = Location.getInstance(row, column-1);
+					if(!cage.getCellLocations().contains(west)){
+						cageSet.add(grid.getCage(west));
+					}
+				}
+				if(row-1>0){
+					Location north = Location.getInstance(row-1, column);
+					if(!cage.getCellLocations().contains(north)){
+						cageSet.add(grid.getCage(north));
+					}
+				}
+				if(column+1<=SIZE){
+					Location east = Location.getInstance(row, column+1);
+					if(!cage.getCellLocations().contains(east)){
+						cageSet.add(grid.getCage(east));
+					}
+				}
+				if(row+1<=SIZE){
+					Location south = Location.getInstance(row+1, column);
+					if(!cage.getCellLocations().contains(south)){
+						cageSet.add(grid.getCage(south));
+					}
+				}
+			}
+			cageList.addAll(cageSet);
+			adjacentCages.put(cage, cageList);
+		}
+		return adjacentCages;
+	}
+
 	public static void main(String[] args) throws FileNotFoundException{
 		List<Cage> cages = CageParser.parseCages("example1.txt");
 		SudokuGrid answer = AnswerParser.parseAnswer("example1Answer.txt");
@@ -52,9 +202,9 @@ public class SudokuGridUI {
 		KillerSudokuSolver solver = new KillerSudokuSolver(grid);
 		solver.solveCagesSpanningExtendRegions();
 		SudokuGridUI gridUI = new SudokuGridUI(grid);
-		solver.setPossibleValuesForCages();
-		solver.solveSingleValueCells();
-		gridUI.makeGrid(grid);
-		
+		//		solver.setPossibleValuesForCages();
+		//		solver.solveSingleValueCells();
+		//		gridUI.makeGrid(grid);
+
 	}
 }
